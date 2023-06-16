6c1fc7bfa1a22352e530543dba0d63427ab428a8
Vicenzo Infusino <v.infusino@astronautics.com>
2023-06-16 13:22:19 -0500

    Added assignment 4 direcotry as well as SQL file for Assignment4

---
+++ assignment4/-- Assignment #4 - Vicenzo Infusino.sql
@@ -0,0 +1,12 @@
+-- Assignment #4 - Vicenzo Infusino
+-- June 25th 2023
+
+-- 1. Find the names of all (students) CS majors.
+
+-- 2. Find the names of all (students) MATH majors who are also in their "Senior" year.
+
+-- 3. Find the names of courses being taught by "Professor Henry".
+
+-- 4. List the IDs and names of courses offered by the "Physics" and "Chemistry departments".
+
+-- 5. Find the names of all faculty members who work in the departments that have less than 20 professors.
